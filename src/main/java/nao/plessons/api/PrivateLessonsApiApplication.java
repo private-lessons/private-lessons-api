0798bff24787eab12272d2e96450a0e180d54e16
noprysk <noprysk@n-ix.com>
2017-11-14 15:24:09 +0200

    Innit Commit

---
+++ src/main/java/nao/plessons/api/PrivateLessonsApiApplication.java
@@ -0,0 +1,12 @@
+package nao.plessons.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateLessonsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateLessonsApiApplication.class, args);
+	}
+}
